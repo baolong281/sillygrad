ac99cc698223a145f5193242487417931102444b
dylan h <baolong281@gmail.com>
2023-09-28 22:56:10 -0500

    new: making train dir makefile

--- Makefile
+++ Makefile
@@ -1,31 +1,31 @@
-cc=clang++
+CC=g++
 CFLAGS=-g -std=c++17
 
 main: engine.o main.o nn.o
-	clang++ $(CFLAGS) nn.o main.o engine.o -o main
+	$(CC) $(CFLAGS) nn.o main.o engine.o -o main
 	./main
 
 test: engine.o test.o nn.o
-	clang++ $(CFLAGS) -lgtest nn.o test.o engine.o -o testing
+	$(CC) $(CFLAGS) -lgtest nn.o test.o engine.o -o testing
 	./testing
 
 mnist: engine.o nn.o mnist.o
-	clang++ $(CFLAGS) nn.o engine.o mnist.o -o mnist
+	$(CC) $(CFLAGS) -lcurl nn.o engine.o mnist.o -o mnist
 
 mnist.o: mnist.cpp
-	clang++ $(CFLAGS) -c mnist.cpp
+	$(CC) $(CFLAGS) -c mnist.cpp
 
 test.o: ./test/test.cpp
-	clang++ $(CFLAGS) -c -lgtest ./test/test.cpp
+	$(CC) $(CFLAGS) -c -lgtest ./test/test.cpp
 
 main.o: ./test/main.cpp
-	clang++ $(CFLAGS) -c ./test/main.cpp
+	$(CC) $(CFLAGS) -c ./test/main.cpp
 
 engine.o: engine.cpp engine.h
-	clang++ $(CFLAGS) -c engine.cpp 
+	$(CC) $(CFLAGS) -c engine.cpp 
 
 nn.o: nn.cpp nn.h
-	clang++ $(CFLAGS) -c nn.cpp
+	$(CC) $(CFLAGS) -c nn.cpp
 
 clean:
 	rm *.o main
--- mnist.cpp
+++ mnist.cpp
@@ -7,6 +7,59 @@
 #include <iostream>
 #include "engine.h"
 #include "nn.h"
+#include <curl/curl.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+static std::string train_imgs_url = "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz";
+
+inline bool file_exists(const std::string& name) {
+    if (FILE *file = fopen(name.c_str(), "r")) {
+        fclose(file);
+        return true;
+    } else {
+        return false;
+    }   
+}
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
+
+void mkdir(const std::string& path) {
+    if(fs::create_directory(path)) {
+        std::cout << "Created directory " << path << std::endl;
+    } else {
+        std::cout << "Failed to create directory " << path << std::endl;
+    }
+}
+
+void make_data() {
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    std::string out_path = "data/train.gz";
+    curl = curl_easy_init();
+
+    if(!file_exists("data")) {
+        mkdir("data");
+    }
+
+    if (curl) {
+        std::cout << "Downloading MNIST data from " << train_imgs_url << std::endl;
+        fp = fopen(out_path.c_str(),"wb");
+        curl_easy_setopt(curl, CURLOPT_URL, train_imgs_url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        std::cout << res << std::endl;
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+        fclose(fp);
+    }
+}
 
 int main(int argc, char *argv[]) {
 
@@ -16,6 +69,11 @@ int main(int argc, char *argv[]) {
         cuda = true;
     }
 
+    std::string path = "data/";
+    
+    if(!file_exists(path)) {
+        make_data();
+    }
 
     return 0;
 }
